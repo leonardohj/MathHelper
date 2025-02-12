4d7d9312aaa21302bf600c499f6c93241de2be95
Leonardo Rodrigues Cheira BTIS 2023/2026 <5481@eclisboa.net>
2025-02-12 16:05:28 +0000

    Terceiro commit

--- src/Projeto_MathHelper/Main.java
+++ src/Projeto_MathHelper/Main.java
@@ -4,146 +4,99 @@ package Projeto_MathHelper;
 import java.util.Scanner;
 
 public class Main {
-    public static int Menu(Scanner scanner)
-    {
-        System.out.println("+-------+-----------------------------------+-------+");
-        System.out.println("+\\     /+            Math Helper            +\\     /+");
-        System.out.println("+ \\   / +-----------------------------------+ \\   / +");
-        System.out.println("+  \\ /  +                                   +  \\ /  +");
-        System.out.println("+   X   + <1> Soma                          +   X   +");
-        System.out.println("+  / \\  +                                   +  / \\  +");
-        System.out.println("+ /   \\ + <2> Subtração                     + /   \\ +");
-        System.out.println("+ \\   / +                                   + \\   / +");
-        System.out.println("+  \\ /  + <3> Multiplicação                 +  \\ /  +");
-        System.out.println("+   X   +                                   +   X   +");
-        System.out.println("+  / \\  + <4> Divisão                       +  / \\  +");
-        System.out.println("+ /   \\ +                                   + /   \\ +");
-        System.out.println("+ \\   / + <5> Proximo número                + \\   / +");
-        System.out.println("+  \\ /  +                                   +  \\ /  +");
-        System.out.println("+   X   + <6> Juntar nome e apelido        +   X   +");
-        System.out.println("+  / \\  +                                   +  / \\  +");
-        System.out.println("+ /   \\ + <7> Maior numero                  + /   \\ +");
-        System.out.println("+/     \\+                                   +/     \\+");
-        System.out.println("+-------+ <8> Média dos números             +-------+");
-        System.out.println("+-------+                                   +-------+");
-        System.out.println("+//////// <0> Sair                          \\\\\\\\\\\\\\\\+");
-        System.out.println("+\\\\\\\\\\\\\\\\___________________________________////////+");
-        System.out.println("+-------+-----------------------------------+-------+\n");
 
-        int menu = scanner.nextInt();
-        return menu;
-    }
-
-    public static boolean VoltarAoMenu(Scanner scanner)
-    {
-        System.out.println("Deseja voltar ao menu? (S\\N)");
-            String resposta = scanner.next();
-            int contador = 0;
-            while(contador == 0) {
-                if (resposta.equals("S")) {
-                    contador++;
-                    return true;
-
-                }
-                if (resposta.equals("N")) {
-                    contador++;
-                    System.out.println("Obrigada por usar o meu programa!");
-                    return false;
-
-                } else {
-                    System.out.println("Resposta inválida!\nTenta novamente...");
-                }
-            }
-            return false;
-    }
-    public static void InserirNumeros(Scanner scanner, int quantosNumeros, int[] numeros)
-    {
-        System.out.println("Ok! Insira os " + quantosNumeros + " numeros.");
-        for(int i = 0; i < quantosNumeros; i++)
-        {
-            numeros[i] = scanner.nextInt();
-        }
-    }
-    public static void InserirNumerosDouble(Scanner scanner, int quantosNumeros, double[] numeros)
-    {
-        System.out.println("Ok! Insira os " + quantosNumeros + " numeros.");
-        for(int i = 0; i < quantosNumeros; i++)
-        {
-            numeros[i] = scanner.nextInt();
-        }
-    }
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    int numero = 0;
+    int numero;
     int[] numeros = new int[50];
     double[] numerosDouble = new double[50];
-    int quantosNumeros = 0;
+    int quantosNumeros;
     String nome, apelido;
-    int menu = 0;
+    int menu;
         boolean voltar = true;
     do {
 
-        if(voltar == false)
+        if(!voltar)
         {
             break;
         }
-        menu = Menu(scanner);
+        menu = MathHelper.Menu(scanner);
         switch (menu) {
+            case 0:
+                System.out.println("\nObrigada por usar o meu programa!");
+                break;
             case 1:
-                System.out.println("Quantos numeros quer somar?");
+                System.out.println("\nQuantos numeros quer somar?");
                 quantosNumeros = scanner.nextInt();
-                InserirNumeros(scanner, quantosNumeros, numeros);
-                System.out.println("\nA soma dos numeros inseridos foi: " + MathHelper.soma(numeros) + "!");
-                voltar = VoltarAoMenu(scanner);
+                if(quantosNumeros <= 1)
+                {
+                    System.out.println("Não podes somar só 1 numero...");
+                }
+                else
+                {
+                    MathHelper.InserirNumeros(scanner, quantosNumeros, numeros);
+                    System.out.println("\nA soma dos numeros inseridos foi: " + MathHelper.soma(numeros, quantosNumeros) + "!");
+                }
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 2:
-                System.out.println("Insira 2 numeros para se subtrairem!");
                 quantosNumeros = 2;
-                InserirNumeros(scanner, quantosNumeros, numeros);
+                MathHelper.InserirNumeros(scanner, quantosNumeros, numeros);
                 System.out.println("\nA subtração dos numeros inseridos foi: " + MathHelper.subtracao(numeros) + "!");
-                voltar = VoltarAoMenu(scanner);
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 3:
-                System.out.println("Insira 2 numeros para se multiplicarem!");
                 quantosNumeros = 2;
-                InserirNumeros(scanner, quantosNumeros, numeros);
+                MathHelper.InserirNumeros(scanner, quantosNumeros, numeros);
                 System.out.println("\nA multiplicação dos numeros inseridos foi: " + MathHelper.multiplicacao(numeros) + "!");
-                voltar = VoltarAoMenu(scanner);
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 4:
-                System.out.println("Insira 2 numeros para se dividirem!");
                 quantosNumeros = 2;
-                InserirNumeros(scanner, quantosNumeros, numeros);
+                MathHelper.InserirNumerosDouble(scanner, quantosNumeros, numerosDouble);
                 double divisaofeita = MathHelper.divisao(numerosDouble);
-                if
-                System.out.println("\nA divisão dos numeros inseridos foi: " + MathHelper.divisao(numerosDouble) + "!");
-                voltar = VoltarAoMenu(scanner);
+                if(divisaofeita == 0)
+                {
+                    System.out.println("Erro! Os numeros inseridos não podem ser divididos!");
+                }
+                else
+                {
+                    System.out.println("\nA divisão dos numeros inseridos foi: " + MathHelper.divisao(numerosDouble) + "!");
+                }
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 5:
                 System.out.println("\nQual é o número que quer inserir?");
                 numero = scanner.nextInt();
                 System.out.println("O numero a seguir ao numero " + numero + " é " + MathHelper.numeroSeguinte(numero) + "!");
-                voltar = VoltarAoMenu(scanner);
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 6:
                 System.out.println("\nInsira o nome!");
-                nome = scanner.nextLine();
+                nome = scanner.next();
                 System.out.println("Insira o apelido");
-                apelido = scanner.nextLine();
+                apelido = scanner.next();
                 System.out.println("O nome junto fica: " + MathHelper.juntarNome(nome, apelido));
-                voltar = VoltarAoMenu(scanner);
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 7:
                 System.out.println("\nQuer inserir quantos numeros? (Max 50)");
                 quantosNumeros = scanner.nextInt();
-                InserirNumeros(scanner, quantosNumeros, numeros);
+                MathHelper.InserirNumeros(scanner, quantosNumeros, numeros);
                 System.out.println("\nO maior numero que foi inserido é o: " + MathHelper.maior(numeros, quantosNumeros) + "!");
-                voltar = VoltarAoMenu(scanner);
+                voltar = MathHelper.VoltarAoMenu(scanner);
                 break;
             case 8:
-
-
+                System.out.println("\nQuer inserir quantos numeros? (Max 50)");
+                quantosNumeros = scanner.nextInt();
+                MathHelper.InserirNumerosDouble(scanner, quantosNumeros, numerosDouble);
+                System.out.println("\nA média dos numeros inseridos é: " + MathHelper.media(numerosDouble, quantosNumeros) + "!");
+                voltar = MathHelper.VoltarAoMenu(scanner);
+                break;
             default:
+                System.out.println("\nResposta inválida!");
+                voltar = MathHelper.VoltarAoMenu(scanner);
+                break;
         }
     }while(menu != 0);
     }
--- src/Projeto_MathHelper/MathHelper.java
+++ src/Projeto_MathHelper/MathHelper.java
@@ -1,19 +1,96 @@
 package Projeto_MathHelper;
 
+import java.util.Scanner;
+
 public class MathHelper {
+
+    public static int Menu(Scanner scanner)
+    {
+        System.out.println("+-------+-----------------------------------+-------+");
+        System.out.println("+\\     /+            Math Helper            +\\     /+");
+        System.out.println("+ \\   / +-----------------------------------+ \\   / +");
+        System.out.println("+  \\ /  +                                   +  \\ /  +");
+        System.out.println("+   X   + <1> Soma                          +   X   +");
+        System.out.println("+  / \\  +                                   +  / \\  +");
+        System.out.println("+ /   \\ + <2> Subtração                     + /   \\ +");
+        System.out.println("+ \\   / +                                   + \\   / +");
+        System.out.println("+  \\ /  + <3> Multiplicação                 +  \\ /  +");
+        System.out.println("+   X   +                                   +   X   +");
+        System.out.println("+  / \\  + <4> Divisão                       +  / \\  +");
+        System.out.println("+ /   \\ +                                   + /   \\ +");
+        System.out.println("+ \\   / + <5> Proximo número                + \\   / +");
+        System.out.println("+  \\ /  +                                   +  \\ /  +");
+        System.out.println("+   X   + <6> Juntar nome e apelido         +   X   +");
+        System.out.println("+  / \\  +                                   +  / \\  +");
+        System.out.println("+ /   \\ + <7> Maior numero                  + /   \\ +");
+        System.out.println("+/     \\+                                   +/     \\+");
+        System.out.println("+-------+ <8> Média dos números             +-------+");
+        System.out.println("+-------+                                   +-------+");
+        System.out.println("+//////// <0> Sair                          \\\\\\\\\\\\\\\\+");
+        System.out.println("+\\\\\\\\\\\\\\\\___________________________________////////+");
+        System.out.println("+-------+-----------------------------------+-------+\n");
+
+        int menu = scanner.nextInt();
+        return menu;
+    }
+
+    public static boolean VoltarAoMenu(Scanner scanner)
+    {
+        System.out.println("Deseja voltar ao menu? (S\\N)");
+        String resposta = scanner.next();
+
+        while(true) {
+            if (resposta.equals("S")) {
+
+                return true;
+
+            }
+            if (resposta.equals("N")) {
+
+                System.out.println("Obrigada por usar o meu programa!");
+                return false;
+
+            } else {
+                System.out.println("Resposta inválida!\nTenta novamente...");
+                resposta = scanner.next();
+            }
+        }
+
+    }
+
+    public static void InserirNumeros(Scanner scanner, int quantosNumeros, int[] numeros)
+    {
+        System.out.println("\nOk! Insira os " + quantosNumeros + " numeros.");
+        for(int i = 0; i < quantosNumeros; i++)
+        {
+            numeros[i] = scanner.nextInt();
+        }
+    }
+
+    public static void InserirNumerosDouble(Scanner scanner, int quantosNumeros, double[] numerosDouble)
+    {
+        System.out.println("\nOk! Insira os " + quantosNumeros + " numeros.");
+        for(int i = 0; i < quantosNumeros; i++)
+        {
+            numerosDouble[i] = scanner.nextDouble();
+        }
+    }
 
     public static int numeroSeguinte(int numero)
     {
         return numero + 1;
     }
+
     public static int subtracao(int[] numeros)
     {
     return numeros[0] - numeros[1];
     }
+
     public static int multiplicacao(int[] numeros)
     {
         return numeros[0] * numeros[1];
     }
+
     public static double divisao(double[] numeros)
     {
         if(numeros[0] == 0 || numeros[1] == 0)
@@ -23,6 +100,7 @@ public class MathHelper {
         }
         return numeros[0] / numeros[1];
     }
+
     public static String juntarNome(String nome, String apelido)
     {
         return nome.concat(" " + apelido);
@@ -51,10 +129,11 @@ public class MathHelper {
         }
         return maior;
     }
-    public static int soma(int[] numeros)
+
+    public static int soma(int[] numeros, int quantosNumeros)
     {
     int soma = 0;
-    for(int i : numeros)
+    for(int i = 0; i < quantosNumeros; i++)
     {
         soma += numeros[i];
     }
@@ -68,6 +147,6 @@ public class MathHelper {
         {
             soma += numeros[i];
         }
-        return soma / quantosNumeros;
+        return soma / (double)quantosNumeros;
     }
 }
